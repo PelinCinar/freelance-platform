f47783e1784cf2c8cb67b83e50924a7a3b260317
Pelin <108188410+PelinCinar@users.noreply.github.com>
2025-03-14 18:08:58 +0300

    Added Swagger documentation for login and register APIs

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
-require("dotenv").config(); 
-const express = require("express"); 
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authRoutes = require("./src/routes/authRoutes.js")
-const cookieParser = require('cookie-parser');
+const authRoutes = require("./src/routes/authRoutes.js");
+const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express"); //swaggeruiyi kullanmak için
+const swaggerSpec = require("./src/config/swaggerConfig.js");
 
-const app = express(); 
+const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+
+//Swagger dokümantasyonu
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
